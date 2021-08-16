9c28d24d424b32fc1bab203e691b47014b015ada
jenning_ho <j3nnning@gmail.com>
2021-08-17 01:30:50 +0800

    update contact card mobile layout

--- scully.m-oscar-gallery.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.config = void 0;
-exports.config = {
-  projectRoot: './src',
-  projectName: 'm-oscar-gallery',
-  outDir: './dist/static',
-  routes: {}
-};
