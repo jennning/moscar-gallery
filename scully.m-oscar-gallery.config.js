255722c71a556654be9b691c7f4c834b1e600764
jenning_ho <j3nnning@gmail.com>
2021-08-17 01:19:17 +0800

    update contacts map link

---
+++ scully.m-oscar-gallery.config.js
@@ -0,0 +1,9 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.config = void 0;
+exports.config = {
+  projectRoot: './src',
+  projectName: 'm-oscar-gallery',
+  outDir: './dist/static',
+  routes: {}
+};
