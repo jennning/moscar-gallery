3763a376af0d3a30812aa07556f0020b63fdd697
jenning_ho <j3nnning@gmail.com>
2021-10-16 19:55:18 +0800

    implement plans facility preview

--- src/app/components/plans/plans.component.ts
+++ src/app/components/plans/plans.component.ts
@@ -5,6 +5,77 @@ import {
   ViewEncapsulation
 } from '@angular/core';
 
+const facilityPreviewData = [
+  {
+    id: '19',
+    name: '50m Olympic-length Pool',
+    img: '12',
+    x: 27,
+    y: 57.2
+  },
+  {
+    id: '8',
+    name: 'Gym Room',
+    img: '10',
+    x: 33.4,
+    y: 21.4
+  },
+  {
+    id: '25',
+    name: '100m Starlight Walk',
+    img: '14',
+    x: 40.8,
+    y: 63.5
+  },
+  {
+    id: '16',
+    name: 'The Stream',
+    img: '15',
+    x: 45.2,
+    y: 48.8
+  },
+  {
+    id: '3',
+    name: 'BBQ Pavilion',
+    img: '11',
+    x: 40.8,
+    y: 1.6
+  },
+  {
+    id: '31',
+    name: 'Children Play Room',
+    img: '4',
+    x: 39.5,
+    y: 86.6
+  },
+  {
+    id: '5',
+    name: 'Multi Purpose Hall',
+    img: '16',
+    x: 71.4,
+    y: 6.1
+  },
+  {
+    id: '32',
+    name: 'Games Room',
+    img: '6',
+    x: 23.6,
+    y: 87.4
+  },
+  {
+    id: '26',
+    name: 'Co-working Space',
+    img: '5',
+    x: 66,
+    y: 68
+  }
+];
+
+const facilityPreviews = facilityPreviewData.map(x => ({
+  ...x,
+  src: `assets/images/gallery/m-oscar-${x.img}.jpg`
+}));
+
 @Component({
   selector: 'app-plans',
   templateUrl: './plans.component.html',
@@ -55,6 +126,8 @@ export class PlansComponent {
     'Gourmet Kitchen',
     'Gourmet Lounge'
   ];
+
+  facilityPreviews = facilityPreviews;
 
   units = [
     {
